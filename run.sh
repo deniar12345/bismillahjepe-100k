09e3a713dc2edfb2c116bcb011b7f33798fb9335
deniar <62832332+deniar12345@users.noreply.github.com>
2024-03-23 08:41:57 +0700

    run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-32c93f00-2a03-4c6a-b736-33ac13d90ead/)
 
 
 echo "$curl_output" >> Update.tsx
